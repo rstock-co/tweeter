873d27158fcca1d566b893b6f9571d34ebbabc6c
Richard Stock <rstock.co@gmail.com>
2022-09-13 15:33:35 -0600

    Add character counter function with feature to auto resize textarea height

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,22 @@
-$(() =>{
-    console.log("Ready!")
+$(() => {
+  /**
+   *  Character Counter
+   *  Counts the number of remaining characters in tweet
+   *  Expands `textarea` box height if tweet is more than one line
+   */
+
+  $("textarea").on("input", function () {
+    const input = $(this).val();
+    const numChars = input.length;
+    let remainingChars = 140 - numChars;
+    const counter = $('output[name="counter"]');
+    counter.html(remainingChars);
+
+    const scrollHeight = $("textarea").prop("scrollHeight");
+    if (scrollHeight === 35) {
+      $("#tweet-text").css("height", "40px");
+    } else {
+      $("#tweet-text").css("height", `${scrollHeight + 5}px`);
+    }
   });
+});
